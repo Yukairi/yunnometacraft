9da3319f19d09853d5af0b143dce031cef55126b
Yukairi <144792685+Yukairi@users.noreply.github.com>
2023-11-04 19:48:41 +0800

    Create tokenonnuy.sol
    create token

---
+++ tokenonnuy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
+
+contract YunnoToken is ERC20, Ownable {
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        require(amount <= 10, "Burn amount exceeds limit of 10");
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+}
